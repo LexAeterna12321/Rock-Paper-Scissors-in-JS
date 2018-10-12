cd51be5847c6336110a878571f4301ddf98073f7
LexAeterna12321 <lexaeterna12321@gmail.com>
2018-10-12 12:05:59 +0200

    result checking function

--- game.js
+++ game.js
@@ -7,7 +7,7 @@ const game = {
 const gameSummary = {
     games: 0,
     wins: 0,
-    loses: 0,
+    losses: 0,
     draws: 0,
 }
 // Player Choice
@@ -25,10 +25,30 @@ const aiChoice = () => {
     const aiHand = hands[Math.floor(Math.random() * hands.length)].dataset.option;
     return aiHand;
 }
+// Result Check
+const resultCheck = (player, ai) => {
+    if (player === ai) {
+        return "draw";
+    } else if (
+        (player === "rock" && ai === "scissors") ||
+        (player === "paper" && ai === "rock") ||
+        (player === "scissors" && ai === "paper")
+    ) {
+        return "win";
+    } else {
+        return "loss";
+    }
+}
 
 // Game Init
 const gameInit = function () {
+    if (!game.playerChoice) {
+        alert("Chose your hand, please")
+    }
     game.aiChoice = aiChoice();
+    const gameResult = resultCheck(game.playerChoice, game.aiChoice);
+
+    document.querySelector("h2 span").innerHTML = gameResult;
 }
 
 document.querySelector(".start").addEventListener("click", gameInit);
